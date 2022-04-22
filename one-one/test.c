9857a60883bd1d03dcbafbc56c3872f0b562831d
Mudit Bapna <bapnamudit03@gmail.com>
2022-04-22 21:28:15 +0530

    one one testing

--- one-one/test.c
+++ one-one/test.c
@@ -27,36 +27,36 @@ int Test(int retval){
     return retval;
 }
 
-void *thread1(){
+void *func1(){
     printf("In THREAD1\n");
     int r = 1;
     i = &r;
     return i;
 }
 
-void *thread2(){
+void *func2(){
     int r = 1010;
     thread_exit(&r);
 }
 
-void *thread3(){
+void *func3(){
     int r = 1;
     i = &r;
     return i;
 }
 
-void *thread4(){
+void *func4(){
     printf("In thread having infinite loop\n");
     int r = 30;
     mrthread_t t3;
     while(infinite);
-    thread_create(&t3, thread3, NULL);
+    thread_create(&t3, func3, NULL);
     thread_join(t3, NULL);
     int *p = &r;
     return p;
 }
 
-void *thread5(){
+void *func5(){
     mask = 1;
     while(mask);
 }
@@ -67,9 +67,9 @@ void sigusr1_handler(){
 }
 
 int main(){
-    LINE;
+    PATTERN;
     printf("1] Thread Create Testing\n");
-    LINE;
+    PATTERN;
     printf("Test 1 --> Start Routine zero\n");{
         mrthread_t t1;
         to_fail(thread_create( &t1, NULL , NULL));
@@ -77,21 +77,21 @@ int main(){
     printf("Test 2 --> Create thread with invalid arguments\n");{
         mrthread_t t1;
         to_fail(thread_create(NULL, NULL, NULL));
-        to_fail(thread_create(NULL, thread1, NULL));
+        to_fail(thread_create(NULL, func1, NULL));
     }
     printf("Test 3 --> Create thread with default attributes\n");{
         mrthread_t tid;
-        Test(thread_create(&tid, thread1, NULL));
+        Test(thread_create(&tid, func1, NULL));
         Test(thread_join(tid, NULL));
         PASSEDTEST;
     }
-    LINE;
+    PATTERN;
     printf("2] Thread Join Testing\n");
-    LINE;
+    PATTERN;
     printf("Test 1 --> Invalid mrthread_t passed\n");
     {
         mrthread_t tid1;
-        Test(thread_create(&tid1, thread1, NULL));
+        Test(thread_create(&tid1, func1, NULL));
         mrthread_t t2;
         t2 = 111;
         to_fail(thread_join(t2, NULL));
@@ -99,14 +99,14 @@ int main(){
 
     printf("Test 2 --> Joining on already joined thread\n");{
         mrthread_t tid2;
-        Test(thread_create(&tid2, thread1, NULL));
+        Test(thread_create(&tid2, func1, NULL));
         Test(thread_join(tid2, NULL));
         to_fail(thread_join(tid2, NULL));
     }
     printf("Test 3 --> Joining on thread and getting the thread value\n");
     {
         mrthread_t tid3;
-        Test(thread_create(&tid3, thread2, NULL));
+        Test(thread_create(&tid3, func2, NULL));
         void *ret;
         Test(thread_join(tid3, &ret));
         printf("Expected return value: 1010\n");
@@ -120,7 +120,7 @@ int main(){
     {
         mrthread_t tid[5];
         for(int i = 0; i < 5; i++) {
-            Test(thread_create(&tid[i], thread3, NULL));
+            Test(thread_create(&tid[i], func3, NULL));
             printf("thread %d created\n", i);
         }
         for(int i = 0; i < 5; i++) {
@@ -130,14 +130,14 @@ int main(){
         PASSEDTEST;
     }
 
-    LINE;
+    PATTERN;
     printf("2] Thread Exit Testing\n");
-    LINE;
+    PATTERN;
     printf("Test 1 --> Created Thread Uses Return To Exit\n");{
         void *ret;
         mrthread_t tid;
 
-        Test(thread_create(&tid, thread1, NULL));
+        Test(thread_create(&tid, func1, NULL));
         Test(thread_join(tid, &ret));
         printf("Expected return value: 1\n");
         printf("Actual return value: %d\n", *(int*)ret);
@@ -151,7 +151,7 @@ int main(){
         void *ret;
         mrthread_t tid;
 
-        Test(thread_create(&tid, thread2, NULL));
+        Test(thread_create(&tid, func2, NULL));
         Test(thread_join(tid, &ret));
         printf("Expected Exit Status is %d\n", 1010);
         printf("Actual   Exit Status is %d\n", *(int*) ret);
@@ -161,9 +161,9 @@ int main(){
             FAILEDTEST
     }
 
-    LINE;
+    PATTERN;
     printf("3] Thread Kill Testing\n");
-    LINE;
+    PATTERN;
     printf("Test 1 --> Send invalid signal\n");
     {
         mrthread_t tid;
@@ -171,7 +171,7 @@ int main(){
         action.sa_handler = sigusr1_handler;
         sigaction(SIGUSR1, &action, NULL);
         infinite = 1;
-        Test(thread_create(&tid, thread4, NULL));
+        Test(thread_create(&tid, func4, NULL));
         Test(thread_kill(tid, -1));
         infinite = 0;
         Test(thread_join(tid, NULL));
@@ -186,7 +186,7 @@ int main(){
         action.sa_handler = sigusr1_handler;
         sigaction(SIGUSR1, &action, NULL);
         infinite = 1;
-        Test(thread_create(&tid, thread4, NULL));
+        Test(thread_create(&tid, func4, NULL));
         Test(thread_kill(tid, SIGUSR1));
         Test(thread_join(tid, &ret));
         if(*(int*)ret == 30)
@@ -198,7 +198,7 @@ int main(){
     printf("Test 3 --> Checking signal handling for SIGTSTP SIGCONT SIGKILL\n");
     {
         mrthread_t tid;
-        Test(thread_create(&tid, thread5, NULL));
+        Test(thread_create(&tid, func5, NULL));
         printf("Sending SIGTSTP signal\n");
         Test(thread_kill(tid, SIGTSTP));
         printf("Sending SIGCONT signal\n");
